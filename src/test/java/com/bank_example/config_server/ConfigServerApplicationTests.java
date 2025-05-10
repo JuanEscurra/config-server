7208004c5b7ffcf034aa962fa3c6deae08c45083
Juan Alexander Escurra Rodriguez <jescurrr@emeal.nttdata.com>
2025-05-10 11:06:27 -0500

    first commit

---
+++ src/test/java/com/bank_example/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank_example.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
